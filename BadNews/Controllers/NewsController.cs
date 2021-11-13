485f059048eec3f8a003e752deacad0cb3bb5409
Timofey Belov <tvb060201@gmail.com>
2021-11-13 17:26:32 +0500

    Tasks 4.1, 4.2

--- BadNews/Controllers/NewsController.cs
+++ BadNews/Controllers/NewsController.cs
@@ -13,14 +13,14 @@ namespace BadNews.Controllers
             this.newsModelBuilder = newsModelBuilder;
         }
 
-        public IActionResult Index([FromQuery] int pageIndex = 0)
+        public IActionResult Index(int? year, int pageIndex = 0)
         {
-            var model = newsModelBuilder.BuildIndexModel(pageIndex, true, null);
+            var model = newsModelBuilder.BuildIndexModel(pageIndex, true, year);
             return View(model);
         }
 
         // [HttpGet("/news/fullarticle/{id}")]
-        public IActionResult FullArticle([FromRoute] Guid id)
+        public IActionResult FullArticle(Guid id)
         {
             var model = newsModelBuilder.BuildFullArticleModel(id);
             if (model == null)
